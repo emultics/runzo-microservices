6509e1aa64b8105b7fb0a05fa81d73a570d13c1c
Suman Manna <manna.suman134@gmail.com>
2025-09-28 16:53:56 +0530

    Feature/activityservice (#8)
    * feat: add logging, exception handling, correlation ID, user registration, and AES crypto support (#1)
    
    * add logback for logger, add exceptions, runzoCoId generation, add interceptor
    
    * add search
    
    * add setup-arch.ps1
    
    ---------
    
    Co-authored-by: Suman Manna <sumanmanna@Sumans-MacBook-Air-2.local>
    
    * Update README.md
    
    * feature/user-microservice (#2)
    
    * add logback for logger, add exceptions, runzoCoId generation, add interceptor
    
    * add search
    
    * add setup-arch.ps1
    
    * Update README.md
    
    * add logback for logger, add exceptions, runzoCoId generation, add interceptor
    
    * add search
    
    * add setup-arch.ps1
    
    ---------
    
    Co-authored-by: Suman Manna <sumanmanna@Sumans-MacBook-Air-2.local>
    
    * add logback for logger, add exceptions, runzoCoId generation, add interceptor
    
    * convert multi module,use common as package, add activity service, userservice, aiservice and eureka server.
    
    * add recommendation-service, get recommendation by ai model, get activities, produce activity to kafka, kafka consumer
    
    * modify gitignore
    
    * modify readme
    
    * Add config server and gateway server (#7)
    
    Co-authored-by: Suman Manna <sumanmanna@Sumans-MacBook-Air-2.local>
    
    ---------
    
    Co-authored-by: Suman Manna <sumanmanna@Sumans-MacBook-Air-2.local>

--- aiservice/src/main/java/com/runzo/aiservice/dto/ActivityResponse.java
+++ aiservice/src/main/java/com/runzo/aiservice/dto/ActivityResponse.java
@@ -1,5 +1,6 @@
 package com.runzo.aiservice.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,16 +14,34 @@ import java.util.Map;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ActivityResponse {
-    private String Id;
-    private String userId;
-    private ActivityType type;
-    private Integer duration;
-    private double calories;
-    private Double distanceKm;
-    private LocalDateTime startTime;
-    private LocalDateTime endTime;
-    private Map<String, Object> additionalMetrics;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
 
+    @JsonProperty("id")
+    private String Id;
+
+    @JsonProperty("user_id")
+    private String userId;
+
+    private ActivityType type;
+
+    private Integer duration;
+
+    private double calories;
+
+    @JsonProperty("distance_km")
+    private Double distanceKm;
+
+    @JsonProperty("start_time")
+    private LocalDateTime startTime;
+
+    @JsonProperty("end_time")
+    private LocalDateTime endTime;
+
+    @JsonProperty("additional_metrics")
+    private Map<String, Object> additionalMetrics;
+
+    @JsonProperty("created_at")
+    private LocalDateTime createdAt;
+
+    @JsonProperty("updated_at")
+    private LocalDateTime updatedAt;
 }
